a7c044d007c5b4da27434e05e7080f67935d695e
PandeeDev <mayolchris@gmail.com>
2024-11-14 23:51:02 -0500

    Finished tutorial, some bugs, but needed to upload

---
+++ unityTutorial2/Assets/Scripts/HealthCollectible.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class HealthCollectible : MonoBehaviour
+{
+    public AudioClip collectedClip;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        PlayerController controller = other.GetComponent<PlayerController>();
+
+        if (controller != null)
+        {
+            controller.ChangeHealth(1);
+            Destroy(gameObject);
+            controller.PlaySound(collectedClip);
+        }
+
+    }
+
+}
